2441b1aab476d6137dbb4a0aac77d34054bec158
Eko Simanjuntak <eko.fikranta@aux.go-jek.com>
2018-07-19 13:23:00 +0700

    Create main and its test

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/go-squads/genrevan-scheduler/util"
+)
+
+func main() {
+	util.GetRootFolderPath()
+}
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain_ExpectedSuccess(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
