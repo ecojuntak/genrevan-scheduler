177326a56b82b93ed593cda81e391d8029268e73
Eko Simanjuntak <eko.fikranta@aux.go-jek.com>
2018-07-24 10:42:09 +0700

    Enable application to access by cli

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"github.com/go-squads/genrevan-scheduler/util"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-squads/genrevan-scheduler/migration"
+
+	"github.com/urfave/cli"
+
+	"github.com/go-squads/genrevan-scheduler/model"
+	"github.com/go-squads/genrevan-scheduler/router"
 )
 
 func main() {
-	util.GetRootFolderPath()
+	initCLI()
+}
+
+func startServer() {
+	model.SetupDatabase("development")
+	router := router.SetupRouter()
+	http.ListenAndServe(":8000", router)
+}
+
+func initCLI() {
+	cliApp := cli.NewApp()
+	cliApp.Name = "Genrevan Scheduler App"
+	cliApp.Version = "1.0.0"
+	cliApp.Commands = []cli.Command{
+		{
+			Name:        "migrate",
+			Description: "Run database migration",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Migration finished")
+				return migration.RunMigration("development")
+			},
+		},
+		{
+			Name:        "start",
+			Description: "Start REST API Server",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Genreven Scheduler started at port 8000...")
+				startServer()
+				return nil
+			},
+		},
+	}
+
+	cliApp.Run(os.Args)
 }
