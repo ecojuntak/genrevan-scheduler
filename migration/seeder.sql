86c1f73cee5fc189e0106e637318e2cbc45ca0d3
reeechart <ferdinandusrichard@yahoo.co.id>
2018-08-15 16:15:36 +0700

    [farid/reeechart] change seeder sql when migration for testing purposes

--- migration/seeder.sql
+++ migration/seeder.sql
@@ -2,9 +2,9 @@ insert into lxds (name, ip_address) values ('Cluster-1','127.0.0.1');
 insert into lxds (name, ip_address) values ('Cluster-2','127.0.0.2');
 insert into lxds (name, ip_address) values ('Cluster-3','127.0.0.3');
 
-insert into lxcs (name, image, id_lxd) values ('Ruby','xenial64',1);
-insert into lxcs (name, image, id_lxd) values ('GOPAY-Scheduler','xenial64',1);
-insert into lxcs (name, image, id_lxd) values ('GOPAY-Backend','xenial64',2);
+insert into lxcs (name, image, id_lxd, host_port, container_port) values ('Ruby','xenial64',1,2399,80);
+insert into lxcs (name, image, id_lxd, host_port, container_port) values ('GOPAY-Scheduler','xenial64',1,2400,8800);
+insert into lxcs (name, image, id_lxd, host_port, container_port) values ('GOPAY-Backend','xenial64',2,2399,8180);
 
 
 insert into metrics (id_lxd, cpu_usage, memory_usage, counter) values (2,10.00,4096,2);
